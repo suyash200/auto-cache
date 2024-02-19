00f6dafffbe9df20fffd6d2ec993e616e24d9867
suyashLade <suyashlade@gmail.com>
2024-02-19 11:47:42 +0530

    intregrating redis

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { log } from "console";
 import { NextFunction, Request, Response } from "express";
-
+import * as redis from "redis";
 interface Config {
   excludedPaths?: Set<string>;
 }
@@ -14,6 +14,7 @@ export class AutoCache {
 
 
   CachingMiddleware = (req: Request, res: Response, next: NextFunction) => {
+
     const { excludedPaths } = this.config;
     const { path } = req;
     if (excludedPaths?.has(path)) {
